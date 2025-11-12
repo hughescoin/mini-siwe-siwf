f989d35e18c49c4257f78774d1ea1c9cc16cfa08
Patrick Hughes <patrick.hughes@coinbase.com>
2025-11-11 21:01:27 -0300

    fix siwf

--- app/components/siwf/index.tsx
+++ app/components/siwf/index.tsx
@@ -1,15 +1,13 @@
-"use client";
 import React from 'react';
 import '@farcaster/auth-kit/styles.css';
 import { SignInButton } from '@farcaster/auth-kit';
 
-
 export const siwfConfig = {
-    rpcUrl: 'https://mainnet.optimism.io',
-    domain: 'siwe-mini.vercel.app/',
-    siweUri: 'https://siwe-mini.vercel.app/sign-in',
-  };
+  rpcUrl: 'https://mainnet.optimism.io',
+  domain: 'siwe-mini.vercel.app',
+  siweUri: 'https://siwe-mini.vercel.app/sign-in',
+};
   
-  export const Login = () => {
-    return <SignInButton />;
-  };
+export const Login = () => {
+  return <SignInButton />;
+};
