8eff5a3c59494a117f403d88dea42c79d6f67296
Patrick Hughes <patrick.hughes@coinbase.com>
2025-12-01 19:28:42 -0600

    reformat

--- minikit.config.ts
+++ minikit.config.ts
@@ -6,15 +6,18 @@ const ROOT_URL = "https://siwe-mini.vercel.app";
  * @see {@link https://miniapps.farcaster.xyz/docs/guides/publishing}
  */
 export const minikitConfig = {
-    "accountAssociation": {
-      "header": "eyJmaWQiOjE3MzE4LCJ0eXBlIjoiYXV0aCIsImtleSI6IjB4QzZDODNGMzE3NDYxZGQxZjVGZjY3QjIxNzcyZmY2NDIyNkQ3M2IxNiJ9",
-      "payload": "eyJkb21haW4iOiJzaXdlLW1pbmkudmVyY2VsLmFwcCJ9",
-      "signature": "ss/R3tDWzNGrgECfFn1Yd+j/ighMNS8N6JTIW12cnehe6lW8znRO3mo+yMOijWLzizVO4Yaz1JuY2SV0+1fFwBw="
-    },
+  accountAssociation: {
+    header: "eyJmaWQiOjE3MzE4LCJ0eXBlIjoiYXV0aCIsImtleSI6IjB4QzZDODNGMzE3NDYxZGQxZjVGZjY3QjIxNzcyZmY2NDIyNkQ3M2IxNiJ9",
+    payload: "eyJkb21haW4iOiJzaXdlLW1pbmkudmVyY2VsLmFwcCJ9",
+    signature: "ss/R3tDWzNGrgECfFn1Yd+j/ighMNS8N6JTIW12cnehe6lW8znRO3mo+yMOijWLzizVO4Yaz1JuY2SV0+1fFwBw=",
+  },
+  baseBuilder: {
+    ownerAddress: "0x5993B8F560E17E438310c76BCac1Af3E6DA2A58A",
+  },
   miniapp: {
     version: "1",
-    name: "NotCubey", 
-    subtitle: "Not Your AI Ad Companion", 
+    name: "NotCubey",
+    subtitle: "Not Your AI Ad Companion",
     description: "Ads",
     screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
     iconUrl: `${ROOT_URL}/blue-icon.png`,
@@ -24,19 +27,12 @@ export const minikitConfig = {
     webhookUrl: `https://api.neynar.com/f/app/3c274018-6402-4a47-96ea-1fea72afc408/event`,
     primaryCategory: "social",
     tags: ["marketing", "ads", "quickstart", "waitlist"],
-    heroImageUrl: `${ROOT_URL}/blue-hero.png`, 
+    heroImageUrl: `${ROOT_URL}/blue-hero.png`,
     tagline: "",
     ogTitle: "",
     ogDescription: "",
     ogImageUrl: `${ROOT_URL}/blue-hero.png`,
     noindex: false,
   },
-  baseBuilder: {
-    ownerAddress: "0x5993B8F560E17E438310c76BCac1Af3E6DA2A58A"
-  }
-
 } as const;
-
-
-
 
